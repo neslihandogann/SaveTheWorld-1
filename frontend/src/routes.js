8acda15dfa368ad4461262507ea2a8aae7cd44b3
alicankilic <ali.can.kilic24@hotmail.com>
2022-03-05 21:34:58 +0300

    serhat sana gelsin

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -18,9 +18,9 @@ export const routes = [
     // component: () => import('./views/About.vue')
   },
   {
-    path: '/login',
-    meta: { title: 'Login', transition: 'slide-left' },
-    component: Login,
+    path: '/register',
+    meta: { title: 'Register', transition: 'slide-left' },
+    component: Register,
     // example of route level code-splitting
     // this generates a separate chunk (About.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
