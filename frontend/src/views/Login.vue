d990a0e928f72a97a6d4bcbd32142dd242ffe94f
Serhat Ak <72757288+srhtak@users.noreply.github.com>
2022-03-05 21:56:15 +0300

    alican yakala tatlim

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,6 +1,5 @@
 <script>
-import { defineComponent } from "@vue/runtime-core";
-export default defineComponent({
+export default {
   data() {
     return {
       userData: {
@@ -12,25 +11,18 @@ export default defineComponent({
   methods: {
     onSubmit() {
       this.$appAxios
-        .get("/api/user/login", {
+        .post("/api/user/login", {
           email: this.userData.email,
           password: this.userData.password,
         })
         .then((login_response) => {
-          if (login_response?.data?.length > 0) {
-            console.log(login_response);
-            this.$store.commit("setUser", login_response?.data[0]);
-            this.$router.push({ name: "Home" });
-          } else {
-            alert("username or password failed");
-          }
+          console.log(login_response);
+          this.$store.commit("setUser", this.email);
+          this.$router.push("/dashboard");
         });
     },
-    onSubmit() {
-      this.$router.push("/");
-    },
   },
-});
+};
 </script>
 
 <template>
@@ -67,190 +59,188 @@ export default defineComponent({
           SAVE THE WORLD
         </div>
         <div class="mt-8">
-          <form action="#" autoComplete="off">
-            <div class="flex flex-col mb-2">
-              <div class="flex relative">
-                <span
-                  class="
-                    rounded-l-md
-                    inline-flex
-                    items-center
-                    px-3
-                    border-t
-                    bg-white
-                    border-l border-b border-gray-300
-                    custom-text
-                    shadow-sm
-                    text-sm
-                  "
-                >
-                  <svg
-                    width="15"
-                    height="15"
-                    fill="currentColor"
-                    viewBox="0 0 1792 1792"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M1792 710v794q0 66-47 113t-113 47h-1472q-66 0-113-47t-47-113v-794q44 49 101 87 362 246 497 345 57 42 92.5 65.5t94.5 48 110 24.5h2q51 0 110-24.5t94.5-48 92.5-65.5q170-123 498-345 57-39 100-87zm0-294q0 79-49 151t-122 123q-376 261-468 325-10 7-42.5 30.5t-54 38-52 32.5-57.5 27-50 9h-2q-23 0-50-9t-57.5-27-52-32.5-54-38-42.5-30.5q-91-64-262-182.5t-205-142.5q-62-42-117-115.5t-55-136.5q0-78 41.5-130t118.5-52h1472q65 0 112.5 47t47.5 113z"
-                    ></path>
-                  </svg>
-                </span>
-                <input
-                  type="text"
-                  v-model="userData.email"
-                  class="
-                    rounded-r-lg
-                    flex-1
-                    appearance-none
-                    border border-gray-300
-                    w-full
-                    py-2
-                    px-4
-                    bg-white
-                    custom-text
-                    placeholder-gray-400
-                    shadow-sm
-                    text-base
-                    focus:outline-none
-                    focus:ring-2
-                    focus:ring-green-300
-                    focus:border-transparent
-                  "
-                  placeholder="Email"
-                />
-              </div>
-            </div>
-            <div class="flex flex-col mb-6">
-              <div class="flex relative">
-                <span
-                  class="
-                    rounded-l-md
-                    inline-flex
-                    items-center
-                    px-3
-                    border-t
-                    bg-white
-                    border-l border-b border-gray-300
-                    custom-text
-                    shadow-sm
-                    text-sm
-                  "
-                >
-                  <svg
-                    width="15"
-                    height="15"
-                    fill="currentColor"
-                    viewBox="0 0 1792 1792"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M1376 768q40 0 68 28t28 68v576q0 40-28 68t-68 28h-960q-40 0-68-28t-28-68v-576q0-40 28-68t68-28h32v-320q0-185 131.5-316.5t316.5-131.5 316.5 131.5 131.5 316.5q0 26-19 45t-45 19h-64q-26 0-45-19t-19-45q0-106-75-181t-181-75-181 75-75 181v320h736z"
-                    ></path>
-                  </svg>
-                </span>
-                <input
-                  type="password"
-                  v-model="userData.password"
-                  class="
-                    rounded-r-lg
-                    flex-1
-                    appearance-none
-                    border border-gray-300
-                    w-full
-                    py-2
-                    px-4
-                    bg-white
-                    text-gray-700
-                    placeholder-gray-400
-                    shadow-sm
-                    text-base
-                    focus:outline-none
-                    focus:ring-2
-                    focus:ring-green-300
-                    focus:border-transparent
-                  "
-                  placeholder="Şifre"
-                />
-              </div>
-            </div>
-            <div class="flex justify-between mb-6 -mt-4">
-              <div class="form-check">
-                <input
-                  class="
-                    form-check-input
-                    appearance-none
-                    h-4
-                    w-4
-                    border border-gray-800
-                    rounded-sm
-                    text-green-500
-                    bg-white
-                    checked:bg-green-600
-                    focus:outline-none
-                    transition
-                    duration-200
-                    mt-1
-                    align-top
-                    bg-no-repeat bg-center bg-contain
-                    float-left
-                    mr-2
-                    cursor-pointer
-                  "
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                <label
-                  class="form-check-label inline-block text-gray-800"
-                  for="flexCheckDefault"
-                >
-                  Beni Hatırla
-                </label>
-              </div>
-              <div class="flex">
-                <a
-                  href="#"
-                  class="
-                    inline-flex
-                    text-xs
-                    font-medium
-                    text-red-600
-                    sm:text-sm
-                    dark:text-gray-100
-                    hover:underline
-                    dark:hover:text-white
-                  "
-                >
-                  Şifremi unuttum
-                </a>
-              </div>
-            </div>
-            <div class="flex w-full">
-              <button
-                @click="onSubmit"
+          <div class="flex flex-col mb-2">
+            <div class="flex relative">
+              <span
                 class="
-                  w-full
-                  px-12
-                  py-3
-                  green
-                  hover:bg-green-700
-                  focus:ring-green-500 focus:ring-offset-green-200
+                  rounded-l-md
+                  inline-flex
+                  items-center
+                  px-3
+                  border-t
+                  bg-white
+                  border-l border-b border-gray-300
                   custom-text
-                  transition
-                  ease-in
-                  duration-200
-                  text-center text-base
-                  font-semibold
-                  shadow-md
-                  focus:outline-none focus:ring-2 focus:ring-offset-2
-                  rounded-lg
+                  shadow-sm
+                  text-sm
                 "
               >
-                Giris yap
-              </button>
+                <svg
+                  width="15"
+                  height="15"
+                  fill="currentColor"
+                  viewBox="0 0 1792 1792"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M1792 710v794q0 66-47 113t-113 47h-1472q-66 0-113-47t-47-113v-794q44 49 101 87 362 246 497 345 57 42 92.5 65.5t94.5 48 110 24.5h2q51 0 110-24.5t94.5-48 92.5-65.5q170-123 498-345 57-39 100-87zm0-294q0 79-49 151t-122 123q-376 261-468 325-10 7-42.5 30.5t-54 38-52 32.5-57.5 27-50 9h-2q-23 0-50-9t-57.5-27-52-32.5-54-38-42.5-30.5q-91-64-262-182.5t-205-142.5q-62-42-117-115.5t-55-136.5q0-78 41.5-130t118.5-52h1472q65 0 112.5 47t47.5 113z"
+                  ></path>
+                </svg>
+              </span>
+              <input
+                type="text"
+                v-model="userData.email"
+                class="
+                  rounded-r-lg
+                  flex-1
+                  appearance-none
+                  border border-gray-300
+                  w-full
+                  py-2
+                  px-4
+                  bg-white
+                  custom-text
+                  placeholder-gray-400
+                  shadow-sm
+                  text-base
+                  focus:outline-none
+                  focus:ring-2
+                  focus:ring-green-300
+                  focus:border-transparent
+                "
+                placeholder="Email"
+              />
             </div>
-          </form>
+          </div>
+          <div class="flex flex-col mb-6">
+            <div class="flex relative">
+              <span
+                class="
+                  rounded-l-md
+                  inline-flex
+                  items-center
+                  px-3
+                  border-t
+                  bg-white
+                  border-l border-b border-gray-300
+                  custom-text
+                  shadow-sm
+                  text-sm
+                "
+              >
+                <svg
+                  width="15"
+                  height="15"
+                  fill="currentColor"
+                  viewBox="0 0 1792 1792"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M1376 768q40 0 68 28t28 68v576q0 40-28 68t-68 28h-960q-40 0-68-28t-28-68v-576q0-40 28-68t68-28h32v-320q0-185 131.5-316.5t316.5-131.5 316.5 131.5 131.5 316.5q0 26-19 45t-45 19h-64q-26 0-45-19t-19-45q0-106-75-181t-181-75-181 75-75 181v320h736z"
+                  ></path>
+                </svg>
+              </span>
+              <input
+                type="password"
+                v-model="userData.password"
+                class="
+                  rounded-r-lg
+                  flex-1
+                  appearance-none
+                  border border-gray-300
+                  w-full
+                  py-2
+                  px-4
+                  bg-white
+                  text-gray-700
+                  placeholder-gray-400
+                  shadow-sm
+                  text-base
+                  focus:outline-none
+                  focus:ring-2
+                  focus:ring-green-300
+                  focus:border-transparent
+                "
+                placeholder="Şifre"
+              />
+            </div>
+          </div>
+          <div class="flex justify-between mb-6 -mt-4">
+            <div class="form-check">
+              <input
+                class="
+                  form-check-input
+                  appearance-none
+                  h-4
+                  w-4
+                  border border-gray-800
+                  rounded-sm
+                  text-green-500
+                  bg-white
+                  checked:bg-green-600
+                  focus:outline-none
+                  transition
+                  duration-200
+                  mt-1
+                  align-top
+                  bg-no-repeat bg-center bg-contain
+                  float-left
+                  mr-2
+                  cursor-pointer
+                "
+                type="checkbox"
+                value=""
+                id="flexCheckDefault"
+              />
+              <label
+                class="form-check-label inline-block text-gray-800"
+                for="flexCheckDefault"
+              >
+                Beni Hatırla
+              </label>
+            </div>
+            <div class="flex">
+              <a
+                href="#"
+                class="
+                  inline-flex
+                  text-xs
+                  font-medium
+                  text-red-600
+                  sm:text-sm
+                  dark:text-gray-100
+                  hover:underline
+                  dark:hover:text-white
+                "
+              >
+                Şifremi unuttum
+              </a>
+            </div>
+          </div>
+          <div class="flex w-full">
+            <button
+              @click="onSubmit"
+              class="
+                w-full
+                px-12
+                py-3
+                green
+                hover:bg-green-700
+                focus:ring-green-500 focus:ring-offset-green-200
+                custom-text
+                transition
+                ease-in
+                duration-200
+                text-center text-base
+                font-semibold
+                shadow-md
+                focus:outline-none focus:ring-2 focus:ring-offset-2
+                rounded-lg
+              "
+            >
+              Giris yap
+            </button>
+          </div>
         </div>
         <div class="flex items-center justify-center mt-6">
           <router-link
