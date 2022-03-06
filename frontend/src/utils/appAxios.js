697a8460cc913f68a327d6d2c543bef67cc1f87d
Oğuz Kağan Yağlıoğlu <me@oguzkagan.xyz>
2022-03-06 13:25:04 +0300

    backend env added

--- frontend/src/utils/appAxios.js
+++ frontend/src/utils/appAxios.js
@@ -2,6 +2,6 @@
 import axios from 'axios'
 
 const appAxios = axios.create({
-    baseURL: 'https://cd60-85-104-206-32.ngrok.io',
+    baseURL: process.env.BACKEND_IP,
 });
 export default appAxios;
