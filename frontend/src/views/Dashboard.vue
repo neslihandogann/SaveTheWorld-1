73d8572f6c2aa00074a63bc0403d94b9f4d71ef5
alicankilic <ali.can.kilic24@hotmail.com>
2022-03-06 13:02:00 +0300

    SC

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -3,7 +3,7 @@
     <nav class="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-red-500 mb-3">
     <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
       <div class="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
-        <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white" id="no1" href="#pablo">
+        <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white" id="no1" href="#SolutionChallenge">
           Save The World
         </a>
         <button class="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none" type="button" v-on:click="toggleNavbar()">
@@ -13,22 +13,22 @@
       <div v-bind:class="{'hidden': !showMenu, 'flex': showMenu}" class="lg:flex lg:flex-grow items-center">
         <ul class="flex flex-col lg:flex-row list-none ml-auto">
           <li class="nav-item">
-            <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+            <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#SolutionChallenge">
               <i class="fab fa-facebook-square text-lg leading-lg text-white opacity-75" /><span class="ml-2">NFT MAĞAZASI</span>
             </a>
           </li>
           <li class="nav-item">
-            <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+            <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#SolutionChallenge">
               <i class="fab fa-twitter text-lg leading-lg text-white opacity-75" /><span class="ml-2">DERSLER</span>
             </a>
           </li>
           <li class="nav-item">
-            <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+            <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#SolutionChallenge">
               <i class="fab fa-pinterest text-lg leading-lg text-white opacity-75" /><span class="ml-2">METAVERSE</span>
             </a>
           </li>
           <li class="nav-item">
-            <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+            <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#SolutionChallenge">
               <i class="fab fa-pinterest text-lg leading-lg text-white opacity-75" /><span class="ml-2">BLOG</span>
             </a>
           </li>
